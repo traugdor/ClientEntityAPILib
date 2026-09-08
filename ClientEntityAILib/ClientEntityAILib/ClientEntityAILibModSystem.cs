39b8745184bdab2cdc8ba69cf59238dfebeb639a
Joel Trauger <joel.trauger@gmail.com>
2026-09-08 16:52:06 -0500

    update mod metadata and bump CakeBuild package versions

--- ClientEntityAILib/ClientEntityAILib/ClientEntityAILibModSystem.cs
+++ ClientEntityAILib/ClientEntityAILib/ClientEntityAILibModSystem.cs
@@ -12,17 +12,17 @@ namespace ClientEntityAILib
         // Useful for registering block/entity classes on both sides
         public override void Start(ICoreAPI api)
         {
-            Mod.Logger.Notification("Hello from template mod: " + api.Side);
+            Mod.Logger.Notification("ClientEntityAILib is Active!");
         }
 
         public override void StartServerSide(ICoreServerAPI api)
         {
-            Mod.Logger.Notification("Hello from template mod server side: " + Lang.Get("cliententityailib:hello"));
+            Mod.Logger.Notification("ClientEntityAILib is Active!");
         }
 
         public override void StartClientSide(ICoreClientAPI api)
         {
-            Mod.Logger.Notification("Hello from template mod client side: " + Lang.Get("cliententityailib:hello"));
+            Mod.Logger.Notification("ClientEntityAILib is Active!");
         }
 
     }
